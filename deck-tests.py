aee2bafb13322385a3eedd52e054f6a6986a653a
Steve Dower <steve.dower@python.org>
2022-02-17 15:23:09 +0000

    Fixes #5 by adding bridge ordering for Suit Fixes #6 by adding Value.Joker Fixes #7 by adding Hand class

--- deck-tests.py
+++ deck-tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 import deck
-from deck import Deck, Card
+from deck import Deck, Card, Hand
 
 
 class DeckTests(unittest.TestCase):
@@ -35,6 +35,25 @@ class CardTests(unittest.TestCase):
         c1 = Card(joker=True)
         c2.joker = True
         assert c1 == c2
+        assert c1.value == deck.Value.Joker
+
+    def test_format(self):
+        c1 = Card("♥", 9)
+        c2 = Card("Diamonds", "King")
+        c3 = Card(joker=True)
+        assert format(c1) == "9♥"
+        assert format(c2) == "K♦"
+        assert format(c3) == "Jok"
+
+        assert format(c1, "<4") == "9♥  "
+        assert format(c2, "<4") == "King♦"
+        assert format(c3, "<4") == "Joker"
+        assert format(c1, ">7") == "     9♥"
+        assert format(c2, ">7") == "  King♦"
+        assert format(c3, ">7") == "  Joker"
+        assert format(c1, "<5.5") == "9♥   "
+        assert format(c2, "<5.5") == "King♦"
+        assert format(c3, "<5.5") == "Joker"
 
 
 class PokerHandTests(unittest.TestCase):
@@ -180,6 +199,170 @@ class PokerHandTests(unittest.TestCase):
         )
         h2 = deck.get_poker_hand(cards + [Card("Hearts", 2), Card("Diamonds", "Jack")])
         assert h1 > h2
+
+
+class HandTests(unittest.TestCase):
+    HAND1 = Hand(
+        Card("♥", 2), Card("♦", 11), Card("♥", 3), Card(joker=True), Card("♠", 11)
+    )
+    HAND2 = Hand(
+        Card("♦", 2), Card("♥", 11), Card("♠", 3), Card(joker=True), Card("♠", 11)
+    )
+
+    def test_type_checks(self):
+        with Hand.runtime_type_checks():
+            try:
+                Hand(["!not a card!"])
+            except ValueError as ex:
+                assert "!not a card!" in str(ex)
+            else:
+                assert False, "expected runtime error"
+
+        with Hand.runtime_type_checks(False):
+            try:
+                Hand("!not a card!")
+            except ValueError as ex:
+                assert False, "expected no error"
+
+    def test_deal(self):
+        deck = Deck()
+        hands = deck.deal_hands()
+        assert len(hands) == 2
+        assert len(hands[0]) == 5
+        assert len(hands[1]) == 5
+
+    def test_default_sort(self):
+        hand = Hand(self.HAND1)
+        hand.sort(deck.HandSort.Default)
+        assert [c.value.value for c in hand] == [2, 3, 11, 11, 15]
+        hand.sort(deck.HandSort.Default, reverse=True)
+        assert [c.value.value for c in hand] == [15, 11, 11, 3, 2]
+
+    def test_sort_suit(self):
+        hand = Hand(Card("♦", 2), Card("♠", 2), Card("♣", 2), Card("♥", 2))
+        hand.sort(deck.HandSort.Default)
+        assert "".join(c.suit.value for c in hand) == "♣♦♥♠"
+        hand.sort(deck.HandSort.Default, reverse=True)
+        assert "".join(c.suit.value for c in hand) == "♠♥♦♣"
+
+    def test_poker_sort(self):
+        hand = Hand(self.HAND1)
+        hand.sort(deck.HandSort.Poker)
+        assert [c.value.value for c in hand] == [2, 3, 15, 11, 11]
+        hand.sort(deck.HandSort.Poker, reverse=True)
+        assert [c.value.value for c in hand] == [11, 11, 15, 3, 2]
+
+    def test_intersect_exact(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        # Self-intersect should be the complete set
+        assert set(hand1.intersect(hand1, deck.HandComparison.Exact)) == set(hand1)
+        assert set(hand2.intersect(hand2, deck.HandComparison.Exact)) == set(hand2)
+        # Only exact matches should remain after an intersect
+        assert set(hand1.intersect(hand2, deck.HandComparison.Exact)) == {
+            Card(joker=True),
+            Card("♠", 11),
+        }
+
+    def test_intersect_value(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        hand3 = Hand(Card("♥", 11))
+        expect_hand3 = {Card("♦", 11), Card("♠", 11)}
+        hand4 = Hand(Card(joker=True))
+        with Hand.default_comparison(deck.HandComparison.Values):
+            # Self-intersect should be the complete set
+            assert set(hand1.intersect(hand1)) == set(hand1)
+            assert set(hand2.intersect(hand2)) == set(hand2)
+            # All values exist between hand1 and hand2 and so we get all of them
+            assert set(hand1.intersect(hand2)) == set(hand1)
+            # Only Jacks are retained
+            assert set(hand1.intersect(hand3)) == expect_hand3
+            # Only Jokers are retained
+            assert set(hand1.intersect(hand4)) == set(hand4)
+            # Check bitwise operator
+            assert set(hand1 & hand3) == expect_hand3
+            # Check in-place operator
+            h1 = Hand(hand1)
+            h1 &= hand3
+            assert set(h1) == expect_hand3
+            assert len(hand1) != len(h1)
+
+    def test_intersect_suit(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        hand3 = Hand(Card("♥", 11))
+        expect_hand3 = {Card("♥", 2), Card("♥", 3)}
+        hand4 = Hand(Card(joker=True))
+        with Hand.default_comparison(deck.HandComparison.Suits):
+            # Self-intersect should be the complete set
+            assert set(hand1.intersect(hand1)) == set(hand1)
+            assert set(hand2.intersect(hand2)) == set(hand2)
+            # All suits exist between hand1 and hand2 and so we get all of them
+            assert set(hand1.intersect(hand2)) == set(hand1)
+            # Only Hearts are retained
+            assert set(hand1.intersect(hand3)) == expect_hand3
+            # Only Jokers are retained
+            assert set(hand1.intersect(hand4)) == set(hand4)
+            # Check bitwise operator
+            assert set(hand1 & hand3) == expect_hand3
+            # Check in-place operator
+            h1 = Hand(hand1)
+            h1 &= hand3
+            assert set(h1) == expect_hand3
+            assert len(hand1) != len(h1)
+
+    def test_union_exact(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        # Self-union should be the original hand
+        assert list(hand1.union(hand1, deck.HandComparison.Exact)) == list(hand1)
+        assert list(hand2.union(hand2, deck.HandComparison.Exact)) == list(hand2)
+        # Exact matches should not duplicate after a union
+        assert list(hand1.union(hand2, deck.HandComparison.Exact)) == [
+            *hand1,
+            *hand2[:3],
+        ]
+
+    def test_union_value(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        hand3 = Hand(Card("♥", 10), Card("♥", 11))
+        expect_hand3 = [*hand1, hand3[0]]
+        hand4 = Hand(Card(joker=True))
+        with Hand.default_comparison(deck.HandComparison.Values):
+            # Self-union should be the complete list
+            assert list(hand1.union(hand1)) == list(hand1)
+            assert list(hand2.union(hand2)) == list(hand2)
+            # All values exist between hand1 and hand2 and so we get all of them
+            assert list(hand1.union(hand2)) == list(hand1)
+            # Only Tens are added
+            assert list(hand1.union(hand3)) == expect_hand3
+            # Nothing is added
+            assert list(hand1.union(hand4)) == list(hand1)
+            # Check bitwise operator
+            assert list(hand1 | hand3) == expect_hand3
+            # Check in-place operator
+            h1 = Hand(c for c in hand1 if c.value == 3)
+            h1 |= hand1
+            assert set(h1) == set(hand1)
+
+    def test_union_suit(self):
+        hand1, hand2 = self.HAND1, self.HAND2
+        hand3 = Hand(Card("♥", 10), Card("♥", 11))
+        expect_hand3 = [*hand1, *hand3]
+        hand4 = Hand(Card(joker=True))
+        with Hand.default_comparison(deck.HandComparison.Suits):
+            # Self-union should be the complete hand
+            assert list(hand1.union(hand1)) == list(hand1)
+            assert list(hand2.union(hand2)) == list(hand2)
+            # All suits exist between hand1 and hand2 and so we get all of them
+            assert list(hand1.union(hand2)) == list(hand1)
+            # Nothing is added
+            assert list(hand1.union(hand3)) == list(hand1)
+            # Nothing is added
+            assert list(hand1.union(hand4)) == list(hand1)
+            # Check bitwise operator
+            assert list(hand1 | hand3) == list(hand1)
+            # Check in-place operator
+            h1 = Hand(c for c in hand1 if c.suit != deck.Suit.Hearts)
+            h1 |= hand1
+            assert set(h1) == set(hand1)
 
 
 if __name__ == "__main__":
--- deck.py
+++ deck.py
@@ -3,9 +3,22 @@
 __version__ = "3.0.0"
 
 import collections
+import contextvars
 import enum
 import itertools
 import random
+
+
+_SUIT_SORT_ORDER = {
+    # Handle unexpected comparisons gracefully
+    "": 10,
+    None: 10,
+    # Suits in bridge ordering
+    "♠": 3,
+    "♥": 2,
+    "♦": 1,
+    "♣": 0,
+}
 
 
 class Suit(enum.Enum):
@@ -13,6 +26,22 @@ class Suit(enum.Enum):
     Spades = "♠"
     Clubs = "♣"
     Diamonds = "♦"
+
+    def __lt__(self, other):
+        x1 = _SUIT_SORT_ORDER[self.value]
+        try:
+            x2 = _SUIT_SORT_ORDER[other.value]
+        except LookupError:
+            x2 = _SUIT_SORT_ORDER[_from_enum(Suit, other).value]
+        return x1 < x2
+
+    def __gt__(self, other):
+        x1 = _SUIT_SORT_ORDER[self.value]
+        try:
+            x2 = _SUIT_SORT_ORDER[other.value]
+        except LookupError:
+            x2 = _SUIT_SORT_ORDER[_from_enum(Suit, other).value]
+        return x1 > x2
 
 
 class Value(enum.IntEnum):
@@ -29,6 +58,21 @@ class Value(enum.IntEnum):
     Jack = 11
     Queen = 12
     King = 13
+    Joker = 15
+
+
+# A mapping from enum values to "nice" names. Only names longer than one
+# character are included, with suitable abbreviations. Users of this map
+# should select the first name from the list of suitable length. Missing
+# values should use str(int(v)) of the value.
+_VALUE_STR_MAP = {
+    Value.Ace: ["Ace", "A"],
+    Value.King: ["King", "Kng", "K"],
+    Value.Queen: ["Queen", "Que", "Q"],
+    Value.Jack: ["Jack", "Jck", "J"],
+    Value.Ten: ["10", "X"],
+    Value.Joker: ["Joker", "Jok", "🤡"],
+}
 
 
 class PokerHand(enum.IntEnum):
@@ -45,17 +89,18 @@ class PokerHand(enum.IntEnum):
 
 
 def _from_enum(enm, v):
-    try:
-        return enm(v)
-    except ValueError:
-        pass
-    try:
-        return getattr(enm, v)
-    except AttributeError:
+    if isinstance(v, str):
         try:
-            return getattr(enm, str(v).capitalize())
-        except AttributeError:
+            return enm(v)
+        except ValueError:
             pass
+        try:
+            return getattr(enm, v)
+        except AttributeError:
+            try:
+                return getattr(enm, str(v).capitalize())
+            except AttributeError:
+                pass
     return enm(v)
 
 
@@ -63,12 +108,14 @@ class Card:
     def __init__(self, suit=None, value=None, joker=False):
         self.joker = joker
         if self.joker:
-            self.suit, self.value = None, None
+            self.suit, self.value = None, Value.Joker
         else:
             if value is None:
                 suit, value = suit
             self.suit = _from_enum(Suit, suit)
             self.value = _from_enum(Value, value)
+        if self.value == Value.Joker:
+            self.joker = True
 
     def __eq__(self, other):
         if not isinstance(other, Card):
@@ -91,6 +138,21 @@ class Card:
     def __str__(self):
         return "Joker" if self.joker else f"{self.value.value}{self.suit.value}"
 
+    def __format__(self, spec):
+        justify, _, width = (spec or ".3").partition(".")
+        width = int(width or 0)
+        if self.joker:
+            for s in _VALUE_STR_MAP[Value.Joker]:
+                if not width or len(s) <= width:
+                    break
+        else:
+            # We include the suit character after the value
+            for s in _VALUE_STR_MAP.get(self.value) or [str(int(self.value))]:
+                if not width or len(s) < width:
+                    break
+            s = s + self.suit.value
+        return format(s, justify)
+
 
 class Deck(collections.deque):
     def __init__(self, include_jokers=True):
@@ -98,7 +160,8 @@ class Deck(collections.deque):
             map(
                 Card,
                 itertools.product(
-                    Suit.__members__.values(), Value.__members__.values()
+                    Suit.__members__.values(),
+                    (v for v in Value.__members__.values() if v != Value.Joker),
                 ),
             )
         )
@@ -113,16 +176,29 @@ class Deck(collections.deque):
 
     deal_from_bottom = collections.deque.popleft
 
+    def deal_hands(self, hands=2, cards=5):
+        if hands * cards > len(self):
+            raise ValueError(
+                "not enough cards to deal {} hands of {}".format(hands, cards)
+            )
+        hand = [Hand() for _ in range(hands)]
+        for _ in range(cards):
+            for h in hand:
+                h.append(self.deal())
+        return hand
+
 
 def aces_high(card):
     """A sort key function to sort aces high."""
     if isinstance(card, Value):
+        if card is None:
+            return 15
         if card == Value.Ace:
             return 14
         return card.value
 
     if card.joker:
-        raise ValueError("Wildcards are not supported")
+        return 15
     if card.value == Value.Ace:
         return 14
     return card.value.value
@@ -137,6 +213,10 @@ def get_poker_hand(cards):
     The best hand sorts last (is greater than other hands).
     """
     cards = sorted(cards, key=aces_high, reverse=True)
+
+    # Any jokers will have sorted to the front
+    if cards and cards[0].joker:
+        raise ValueError("Cannot calculate poker hand including jokers")
 
     if len(cards) > 5:
         return max(map(get_poker_hand, itertools.combinations(cards, 5)))
@@ -187,6 +267,274 @@ def get_poker_hand(cards):
     return (PokerHand.HighCard,) + tuple(
         sorted((aces_high(c) for c in cvalues), reverse=True)
     )
+
+
+class HandSort(enum.Enum):
+    Default = "default"
+    Poker = "poker"
+    Unsorted = "unsorted"
+
+
+class HandComparison(enum.Enum):
+    Exact = "exact"
+    Values = "values"
+    Suits = "suits"
+
+
+_HAND_VALIDATION = contextvars.ContextVar("Hand.runtime_type_checks")
+_HAND_VALIDATION.set(False)
+
+_HAND_ORDER = contextvars.ContextVar("Hand.default_sort")
+_HAND_ORDER.set(HandSort.Default)
+
+_HAND_CMP = contextvars.ContextVar("Hand.default_comparison")
+_HAND_CMP.set(HandComparison.Exact)
+
+
+class Hand:
+    """Represents a hand of cards.
+
+    Used for conveniently comparing and displaying hands."""
+
+    def __init__(self, *cards):
+        self.cards = new_cards = []
+        for c in cards:
+            if isinstance(c, Card):
+                new_cards.append(c)
+            else:
+                new_cards.extend(c)
+        if _HAND_VALIDATION.get():
+            self._check()
+
+    def _check(self):
+        invalid = [c for c in self.cards if not isinstance(c, Card)]
+        if invalid:
+            raise ValueError(
+                "invalid objects in hand: {}".format(
+                    ", ".join(repr(c) for c in invalid)
+                )
+            )
+
+    def append(self, card):
+        """Add a card to the hand."""
+        self.cards.append(card)
+        if _HAND_VALIDATION.get():
+            self._check()
+
+    def extend(self, cards):
+        """Add multiple cards to the hard."""
+        self.cards.extend(cards)
+        if _HAND_VALIDATION.get():
+            self._check()
+
+    def index(self, card_suit_or_value):
+        """Locate the first matching card, suit or value."""
+        # Being passed a whole card is our fast path
+        if isinstance(card_suit_or_value, Card):
+            cmp = _HAND_CMP.get()
+            if cmp == HandComparison.Exact:
+                cards = self.cards
+                for i in range(len(cards)):
+                    if cards[i] == card_suit_or_value:
+                        return i
+                raise LookupError(card_suit_or_value)
+            elif cmp == HandComparison.Values:
+                card_suit_or_value = card_suit_or_value.value
+            elif cmp == HandComparison.Suits:
+                card_suit_or_value = card_suit_or_value.suit
+            else:
+                raise ValueError("unable to compare with {}".format(cmp))
+
+        # Convert int or str to enum types transparently
+        if isinstance(card_suit_or_value, int):
+            try:
+                card_suit_or_value = _from_enum(Value, card_suit_or_value)
+            except ValueError:
+                pass
+        elif isinstance(card_suit_or_value, str):
+            try:
+                card_suit_or_value = _from_enum(Suit, card_suit_or_value)
+            except ValueError:
+                try:
+                    card_suit_or_value = _from_enum(Value, card_suit_or_value)
+                except ValueError:
+                    pass
+
+        # If we now have a searchable type, search for it
+        if isinstance(card_suit_or_value, Value):
+            cards = self.cards
+            for i in range(len(cards)):
+                if cards[i].value == card_suit_or_value:
+                    return i
+        elif isinstance(card_suit_or_value, Suit):
+            cards = self.cards
+            for i in range(len(cards)):
+                if cards[i].suit == card_suit_or_value:
+                    return i
+        raise LookupError(card_suit_or_value)
+
+    def __contains__(self, card_suit_or_value):
+        try:
+            self.index(card_suit_or_value)
+            return True
+        except LookupError:
+            return False
+
+    def intersect(self, other, cmp=None):
+        """Compares two hands by removing cards that are not in 'other'."""
+        cmp = cmp or _HAND_CMP.get()
+        if cmp == HandComparison.Exact:
+            return iter(set(self.cards) & set(other.cards))
+        if cmp == HandComparison.Values:
+            v1 = set(c.value for c in self.cards)
+            v2 = set(c.value for c in other.cards)
+            v12 = v1 & v2
+            return (c for c in self.cards if c.value in v12)
+        if cmp == HandComparison.Suits:
+            s1 = set(c.suit for c in self.cards)
+            s2 = set(c.suit for c in other.cards)
+            s12 = s1 & s2
+            return (c for c in self.cards if c.suit in s12)
+        raise ValueError("cannot compare by {}".format(cmp))
+
+    def __and__(self, other):
+        return type(self)(self.intersect(other))
+
+    def __ibitand__(self, other):
+        self.cards[:] = self.intersect(other)
+
+    def union(self, other, cmp=None):
+        """Combines two hands by adding cards from 'other'."""
+        cmp = cmp or _HAND_CMP.get()
+        yield from iter(self.cards)
+        if cmp == HandComparison.Exact:
+            c1 = set(self.cards)
+            yield from (c for c in other.cards if c not in c1)
+        elif cmp == HandComparison.Values:
+            v1 = set(c.value for c in self.cards)
+            yield from (c for c in other.cards if c.value not in v1)
+        elif cmp == HandComparison.Suits:
+            s1 = set(c.suit for c in self.cards)
+            yield from (c for c in other.cards if c.suit not in s1)
+        else:
+            raise ValueError("cannot compare by {}".format(cmp))
+
+    def __or__(self, other):
+        return type(self)(self.union(other))
+
+    def __ibitor__(self, other):
+        self.cards[:] = self.union(other)
+
+    @staticmethod
+    def _default_sort_key(card):
+        return card.value or 0, card.suit or ""
+
+    @property
+    def _poker_sort_key(self):
+        count = {}
+        for c in self.cards:
+            count[c.value] = count.get(c.value, 0) + 1
+        return lambda c: (count.get(c.value), aces_high(c), c.suit)
+
+    def sorted(self, order=None, *, reverse=False):
+        """Returns a sorted list of cards in this hand."""
+        order = order or _HAND_ORDER.get()
+        if order == HandSort.Default:
+            cards = sorted(self.cards, key=self._default_sort_key, reverse=reverse)
+        elif order == HandSort.Poker:
+            cards = sorted(self.cards, key=self._poker_sort_key, reverse=reverse)
+        elif order == HandSort.Unsorted:
+            pass
+        else:
+            raise ValueError("unable to sort with {}".format(order))
+        return cards
+
+    def sort(self, order=None, *, reverse=False):
+        """Sorts the cards in this hand in place."""
+        order = order or _HAND_ORDER.get()
+        if order == HandSort.Default:
+            self.cards.sort(key=self._default_sort_key, reverse=reverse)
+        elif order == HandSort.Poker:
+            self.cards.sort(key=self._poker_sort_key, reverse=reverse)
+        elif order == HandSort.Unsorted:
+            pass
+        else:
+            raise ValueError("unable to sort with {}".format(order))
+
+    def __format__(self, spec):
+        import re
+
+        if not spec:
+            spec = "4.3desc"
+        strs = []
+        cards = self.cards
+        if spec.casefold().endswith("desc".casefold()):
+            cards = self.sorted(reverse=True)
+            spec = spec[:-4]
+        elif spec.casefold().endswith("asc".casefold()):
+            cards = self.sorted()
+            spec = spec[:-3]
+        return "".join(format(c, spec) for c in cards)
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
+    def __iter__(self):
+        return iter(self.cards)
+
+    def __len__(self):
+        return len(self.cards)
+
+    def __bool__(self):
+        return bool(self.cards)
+
+    def __str__(self):
+        return format(self)
+
+    def __repr__(self):
+        return "<Hand({!r})>".format(self.cards)
+
+    class default_comparison:
+        """A context manager for overriding the default comparison."""
+
+        def __init__(self, cmp=HandComparison.Exact):
+            self._cmp = cmp
+            self._token = None
+
+        def __enter__(self):
+            self._token = _HAND_CMP.set(self._cmp)
+            return self
+
+        def __exit__(self, *exc):
+            _HAND_CMP.reset(self._token)
+
+    class default_sort:
+        """A context manager for overriding the default sort order."""
+
+        def __init__(self, order=HandSort.Default):
+            self._order = order
+            self._token = None
+
+        def __enter__(self):
+            self._token = _HAND_ORDER.set(self._order)
+            return self
+
+        def __exit__(self, *exc):
+            _HAND_ORDER.reset(self._token)
+
+    class runtime_type_checks:
+        """A context manager for enabling runtime type validation."""
+
+        def __init__(self, check=True):
+            self._check = check
+            self._token = None
+
+        def __enter__(self):
+            self._token = _HAND_VALIDATION.set(self._check)
+            return self
+
+        def __exit__(self, *exc):
+            _HAND_VALIDATION.reset(self._token)
 
 
 collections.deck = Deck
